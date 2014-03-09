d7b2714a7558019c21d5d8138d75ab785b117e5c
Marek Moravcik <marek@marek-P53SJ.(none)>
2014-03-09 22:47:37 +0100

    Added GNU GPL v3 license

--- src/GrizzlyServer.cpp
+++ src/GrizzlyServer.cpp
@@ -1,9 +1,20 @@
 /*
- * GrizzlyServer.cpp
- *
- *  Created on: Mar 1, 2014
- *      Author: visio
- */
+C++ server for web-driven administration with module support.
+Copyright (C) 2014  Vladimír Bartošík, Martin Kuzma, Marek Moravčík
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "GrizzlyServer.h"
 #include "tools/file.h"
